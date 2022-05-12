7305df4f2eb9a222686061eba815eda036a230f1
Owen Jones <owen@owenjones.net>
2022-05-12 19:47:42 +0100

    trust all

--- src/app/Http/Middleware/TrustProxies.php
+++ src/app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies = ['172.21.0.4'];
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
