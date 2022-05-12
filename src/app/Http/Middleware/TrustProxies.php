09ac2ac067fbf14c063ec9e4405f458837f00a42
Owen Jones <owen@owenjones.net>
2022-05-12 19:46:30 +0100

    trusted proxy for prod server

--- src/app/Http/Middleware/TrustProxies.php
+++ src/app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = ['172.21.0.4'];
 
     /**
      * The headers that should be used to detect proxies.
