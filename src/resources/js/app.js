d9fcf8e985cc6605d2f71a0995cd6b85fe6f3e5f
Owen Jones <owen@owenjones.net>
2022-05-13 14:59:06 +0100

    include app name in title

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -5,8 +5,10 @@ import { render } from 'react-dom';
 import { createInertiaApp } from '@inertiajs/inertia-react';
 import { InertiaProgress } from '@inertiajs/progress';
 
+const app_name = document.getElementsByTagName('title')[0].innerText;
+
 createInertiaApp({
-    title: (title) => `${title}`,
+    title: (title) => `${title} - ${app_name}`,
     resolve: (name) => require(`./pages/${name}`),
     setup({ el, App, props }) {
         return render(<App {...props} />, el);
--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Photo Trail') }}</title>
+        <title inertia>{{ config('app.name') }}</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&family=Staatliches&display=swap" rel="stylesheet">
