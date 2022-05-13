9f2bc6a99eda744fd3b91082fdfc0b94a880c1b2
Owen Jones <owen@owenjones.net>
2022-05-13 16:14:59 +0100

    App facade was missing

--- src/app/Http/Controllers/TeamController.php
+++ src/app/Http/Controllers/TeamController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\App;
 
 use App\Models\Group;
 use App\Models\Team;
