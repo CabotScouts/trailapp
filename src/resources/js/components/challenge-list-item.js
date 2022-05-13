6b7bcf227b1708e65f130c6ae5f5532787fd092a
Owen Jones <owen@owenjones.net>
2022-05-13 23:06:28 +0100

    UI tweaks prod build

--- src/resources/js/components/challenge-list-item.js
+++ src/resources/js/components/challenge-list-item.js
@@ -3,7 +3,7 @@ import { Link } from '@inertiajs/inertia-react';
 
 export default function ChallengeListItem({ data }) {
   return (
-    <Link preserveScroll href={ route('challenge', data.id) }>
+    <Link href={ route('challenge', data.id) }>
       <div className="p-5 flex items-center border border-b-slate-500">
         <div className="flex-grow pr-5">
           <p className="text-medium text-lg text-slate-800">{ data.name }</p>
--- src/resources/js/layouts/list-frame.js
+++ src/resources/js/layouts/list-frame.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default function ListFrame({ team, group, children }) {
   return (
-    <div className="flex flex-col bg-neutral-100">
-      <div className="flex-none px-5 py-4 bg-purple-900 shadow-sm">
+    <div className="bg-neutral-100">
+      <div className="px-5 py-4 bg-purple-900 shadow-sm">
         <p className="font-medium text-3xl font-serif text-neutral-50">{ team }</p>
         <p className="text-sm text-neutral-100">{ group }</p>
       </div>
 
-      <div className="flex-grow overflow-auto scroll-region">
+      <div>
         { children }
       </div>
     </div>
