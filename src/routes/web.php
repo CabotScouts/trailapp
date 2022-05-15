44f84436cb8fa6eb9ca98c2060effe185d77b941
Owen Jones <owen@owenjones.net>
2022-05-15 20:18:50 +0100

    special routes to login as different teams now accessible in prod by authorised users

--- src/routes/web.php
+++ src/routes/web.php
@@ -10,10 +10,8 @@ use App\Http\Controllers\AdminController;
 Route::get('/', [TeamController::class, 'index'])->middleware(['checkteam'])->name('start');
 Route::post('/create-team', [TeamController::class, 'create'])->name('create-team');
 
-if(App::environment(['local', 'staging'])) {
-  Route::get('/clone/{id}', [TeamController::class, 'clone']);
-  Route::get('/destroy', [TeamController::class, 'destroy'])->middleware(['auth:team']);
-}
+Route::get('/clone/{id}', [TeamController::class, 'clone'])->middleware(['auth:user']);
+Route::get('/destroy', [TeamController::class, 'destroy'])->middleware(['auth:user', 'auth:team']);
 
 Route::get('/trail', [TrailController::class, 'index'])->middleware(['auth:team'])->name('trail');
 Route::get('/challenges/{id}', [TrailController::class, 'viewChallenge'])->middleware(['auth:team'])->name('challenge');
