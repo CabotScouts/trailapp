e78d32ce42648b9d697dcae1969431e3f43666fc
Owen Jones <owen@owenjones.net>
2022-05-15 20:23:01 +0100

    initial dashboard view

---
+++ src/resources/js/layouts/admin/frame.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Frame(props) {
+  return (
+    
+  );
+}
---
+++ src/resources/js/pages/admin/dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Head } from '@inertiajs/inertia-react';
+import Frame from '@/layouts/admin/frame';
+
+export default function Dashboard(props) {
+  return (
+    <>
+      <Head title="Dashboard" />
+    </>
+  );
+}
