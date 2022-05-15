315dc65a45aec8de07fc5bb9c30fa3b8fda475c7
Owen Jones <owen@owenjones.net>
2022-05-15 20:18:02 +0100

    new user model

---
+++ src/app/Models/User.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable {
+
+  public $fillable = ['username', 'password'];
+
+}
