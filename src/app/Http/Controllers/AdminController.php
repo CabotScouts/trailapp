a06583c8d5fca5fe3600f8439f854e4a9226f60a
Owen Jones <owen@owenjones.net>
2022-05-15 20:23:48 +0100

    initial admin routes, can now login, logout, and access dashboard

--- src/app/Http/Controllers/AdminController.php
+++ src/app/Http/Controllers/AdminController.php
@@ -4,6 +4,13 @@ namespace App\Http\Controllers;
 
 use Inertia\Inertia;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\Team;
 
 class AdminController extends Controller {
   
@@ -14,8 +21,17 @@ class AdminController extends Controller {
       ]);
     }
     elseif($request->isMethod('post')) {
-      // do login
-      return "reached form submit";
+      $credentials = $request->validate([
+        'username' => 'required|string|max:255',
+        'password' => 'required|string',
+      ]);
+      
+      if(Auth::guard('user')->attempt($credentials)) {
+        $request->session()->regenerate();
+        return redirect()->route('dashboard');
+      }
+      
+      return back()->withErrors(['username' => 'The username or password is incorrect']);  
     }
     else {
       // how?
@@ -24,11 +40,20 @@ class AdminController extends Controller {
   }
   
   public function logout(Request $request) {
-    
+    Auth::guard('user')->logout();
+    $request->session()->invalidate();
+    $request->session()->regenerateToken();
+    return redirect()->route('login');
   }
   
   public function dashboard(Request $request) {
-    
+    return Inertia::render('admin/dashboard', [
+      'name' => config('app.name'),
+      'teams' => Team::all()->map(fn($team) => [
+        'id' => $team->id,
+        'name' => $team->name
+      ]),
+    ]);
   }
   
 }
