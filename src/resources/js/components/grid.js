2872f16ea5f7b84487e9b2b38fbbdd7c332e3f2b
Owen Jones <owen@owenjones.net>
2022-05-16 13:04:34 +0100

    new grid view component

---
+++ src/resources/js/components/grid.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from '@inertiajs/inertia-react';
+
+export function Grid({ children }) {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-4 p-4">
+      { children }
+    </div>
+  )
+}
+
+export function GridItem({ href, className, children }) {
+  return (
+    <Link href={ href }>
+      <div className={`p-4 rounded-xl ${ className }`}>
+        <div className="mt-6 font-serif text-2xl text-neutral-100">{ children }</div>
+      </div>
+    </Link>
+  )
+}
