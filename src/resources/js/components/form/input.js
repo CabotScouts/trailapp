98db8f327469b1024c2bb0697b22c8d145361574
Owen Jones <owen@owenjones.net>
2022-05-16 22:36:53 +0100

    added value paramater so can pass existing values through

--- src/resources/js/components/form/input.js
+++ src/resources/js/components/form/input.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Input({ title, type, name, onChange, required, placeholder }) {
+export default function Input({ title, type, name, onChange, required, placeholder, value }) {
   return (
     <div>
       <label htmlFor="name" className="block font-medium text-sm text-slate-700">{ title }</label>
@@ -8,6 +8,7 @@ export default function Input({ title, type, name, onChange, required, placehold
         <input
           type={ type }
           name={ name }
+          value={ value }
           className="border-slate-300 focus:border-purple-300 focus:ring focus:ring-purple-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full"
           required={ required }
           placeholder={ placeholder }
