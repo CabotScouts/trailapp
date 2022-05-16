6472464a82f36ef149903b65cd48273c0793158a
Owen Jones <owen@owenjones.net>
2022-05-16 13:03:06 +0100

    fleshed out all admin routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -20,3 +20,24 @@ Route::post('/challenges/{id}/submit', [TrailController::class, 'challengeSubmis
 Route::match(['get', 'post'], '/login', [AdminController::class, 'login'])->middleware(['guest'])->name('login');
 Route::get('/logout', [AdminController::class, 'logout'])->middleware(['auth:user'])->name('logout');
 Route::get('/dashboard', [AdminController::class, 'dashboard'])->middleware(['auth:user'])->name('dashboard');
+
+Route::get('/submissions', [AdminController::class, 'submissions'])->middleware(['auth:user'])->name('submissions');
+Route::get('/submission/{id}', [AdminController::class, 'viewSubmission'])->middleware(['auth:user'])->name('view-submission');
+Route::get('/submission/{id}/accept', [AdminController::class, 'acceptSubmission'])->middleware(['auth:user'])->name('accept-submission');
+Route::get('/submission/{id}/delete', [AdminController::class, 'deleteSubmission'])->middleware(['auth:user'])->name('delete-submission');
+
+Route::get('/teams', [AdminController::class, 'teams'])->middleware(['auth:user'])->name('teams');
+Route::get('/team/{id}', [AdminController::class, 'viewTeam'])->middleware(['auth:user'])->name('view-team');
+Route::match(['get', 'post'], '/team/{id}/delete', [AdminController::class, 'deleteTeam'])->middleware(['auth:user'])->name('delete-team');
+
+Route::get('/challenges', [AdminController::class, 'challenges'])->middleware(['auth:user'])->name('challenges');
+Route::get('/challenge/{id}', [AdminController::class, 'viewChallenge'])->middleware(['auth:user'])->name('view-challenge');
+Route::match(['get', 'post'], '/challenge/new', [AdminController::class, 'addChallenge'])->middleware(['auth:user'])->name('add-challenge');
+Route::match(['get', 'post'], '/challenge/{id}/edit', [AdminController::class, 'editChallenge'])->middleware(['auth:user'])->name('edit-challenge');
+Route::match(['get', 'post'], '/challenge/{id}/delete', [AdminController::class, 'deleteChallenge'])->middleware(['auth:user'])->name('delete-challenge');
+
+Route::get('/groups', [AdminController::class, 'groups'])->middleware(['auth:user'])->name('groups');
+Route::get('/group/{id}', [AdminController::class, 'viewGroup'])->middleware(['auth:user'])->name('view-group');
+Route::match(['get', 'post'], '/group/new', [AdminController::class, 'addGroup'])->middleware(['auth:user'])->name('add-group');
+Route::match(['get', 'post'], '/group/{id}/edit', [AdminController::class, 'editGroup'])->middleware(['auth:user'])->name('edit-group');
+Route::match(['get', 'post'], '/group/{id}/delete', [AdminController::class, 'deleteGroup'])->middleware(['auth:user'])->name('delete-group');
