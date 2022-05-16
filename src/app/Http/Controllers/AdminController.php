61a6112de03403ce7693e9f596cc721d7b9a1a7f
Owen Jones <owen@owenjones.net>
2022-05-16 13:04:19 +0100

    scaffolded out all admin functions

--- src/app/Http/Controllers/AdminController.php
+++ src/app/Http/Controllers/AdminController.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Hash;
 use App\Models\Team;
 
 class AdminController extends Controller {
-  
+
   public function login(Request $request) {
     if($request->isMethod('get')) {
       return Inertia::render('admin/login', [
@@ -25,35 +25,97 @@ class AdminController extends Controller {
         'username' => 'required|string|max:255',
         'password' => 'required|string',
       ]);
-      
+
       if(Auth::guard('user')->attempt($credentials)) {
         $request->session()->regenerate();
         return redirect()->route('dashboard');
       }
-      
-      return back()->withErrors(['username' => 'The username or password is incorrect']);  
+
+      return back()->withErrors(['username' => 'The username or password is incorrect']);
     }
     else {
       // how?
       return redirect()->route('login');
     }
   }
-  
+
   public function logout(Request $request) {
     Auth::guard('user')->logout();
     $request->session()->invalidate();
     $request->session()->regenerateToken();
     return redirect()->route('login');
   }
-  
+
   public function dashboard(Request $request) {
-    return Inertia::render('admin/dashboard', [
-      'name' => config('app.name'),
-      'teams' => Team::all()->map(fn($team) => [
-        'id' => $team->id,
-        'name' => $team->name
-      ]),
-    ]);
+    return Inertia::render('admin/dashboard');
   }
-  
+
+  public function submissions() {
+
+  }
+
+  public function viewSubmission() {
+
+  }
+
+  public function acceptSubmission() {
+
+  }
+
+  public function deleteSubmission() {
+
+  }
+
+  public function teams() {
+
+  }
+
+  public function viewTeam() {
+
+  }
+
+  public function deleteTeam() {
+
+  }
+
+  public function challenges() {
+
+  }
+
+  public function viewChallenge() {
+
+  }
+
+  public function addChallenge() {
+
+  }
+
+  public function editChallenge() {
+
+  }
+
+  public function deleteChallenge() {
+
+  }
+
+  public function groups() {
+
+  }
+
+  public function viewGroup() {
+
+  }
+
+  public function addGroup() {
+
+  }
+
+  public function editGroup() {
+
+  }
+
+  public function deleteGroup() {
+    
+  }
+
 }
