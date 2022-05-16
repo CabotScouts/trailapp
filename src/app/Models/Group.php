d87f72239442129e057a3a69573a614a03cceca2
Owen Jones <owen@owenjones.net>
2022-05-16 22:36:29 +0100

    added fillable columns (name and number) and disabled timestamps

--- src/app/Models/Group.php
+++ src/app/Models/Group.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model {
+  
+  protected $fillable = ['name', 'number'];
+  public $timestamps = false;
+  
   public function teams() {
     return $this->hasMany(Team::class);
   }
