067a24b696afad5c2ff47fd0dce146eb7543aa93
Owen Jones <owen@owenjones.net>
2022-05-16 16:53:51 +0100

    added edit group link

--- src/resources/js/pages/admin/group/list.js
+++ src/resources/js/pages/admin/group/list.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from '@inertiajs/inertia-react';
 import Frame from '@/layouts/admin/frame';
 import ListItem from '@/components/admin/list-item';
+import { PencilIcon } from '@heroicons/react/solid';
 
 export default function List({ groups }) {
   return (
@@ -9,9 +11,12 @@ export default function List({ groups }) {
         <div className="flex-grow pr-5">
           <p className="text-xl text-medium">{ g.name }</p>
         </div>
-        <div className="flex-none">
-          <div className="w-8 rounded-full text-center text-neutral-100 text-medium text-sm p-2 bg-cyan-600">
+        <div className="flex-none flex">
+          <div className="w-8 mr-2 rounded-full text-center text-neutral-100 text-medium text-sm p-2 bg-cyan-600">
             <p>{ g.teams }</p>
+          </div>
+          <div className="w-8 rounded-xl text-center text-neutral-100 text-medium text-sm p-2 bg-emerald-600">
+            <Link href={ route('edit-group', g.id) }><PencilIcon /></Link>
           </div>
         </div>
       </ListItem>) }
