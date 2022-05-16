4449507605b4783bc2a1335d6873c429d72d122d
Owen Jones <owen@owenjones.net>
2022-05-16 13:03:47 +0100

    initial admin dashboard

--- src/resources/js/pages/admin/dashboard.js
+++ src/resources/js/pages/admin/dashboard.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Head } from '@inertiajs/inertia-react';
 import Frame from '@/layouts/admin/frame';
+import { Grid, GridItem } from '@/components/grid';
 
 export default function Dashboard(props) {
   return (
-    <>
-      <Head title="Dashboard" />
-    </>
+    <Frame page="Dashboard">
+      <Grid>
+        <GridItem className="bg-green-600" href={ route('submissions') }>Submissions</GridItem>
+        <GridItem className="bg-orange-600" href={ route('teams') }>Teams</GridItem>
+        <GridItem className="bg-purple-600" href={ route('challenges') }>Challenges</GridItem>
+        <GridItem className="bg-cyan-600" href={ route('groups') }>Groups</GridItem>
+      </Grid>
+    </Frame>
   );
 }
