212120fa6726b5ff3de65edd008c47acff8a98c2
Owen Jones <owen@owenjones.net>
2022-05-16 16:18:21 +0100

    updated route and added edit link

--- src/resources/js/pages/admin/challenge/list.js
+++ src/resources/js/pages/admin/challenge/list.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import { Link } from '@inertiajs/inertia-react';
 import Frame from '@/layouts/admin/frame';
 import ListItem from '@/components/admin/list-item';
+import { PencilIcon } from '@heroicons/react/solid';
 
 export default function List({ challenges }) {
   return (
     <Frame title="Challenges">
-      { challenges.map((c) => <ListItem key={ c.id } target={ route('view-challenge', c.id) }>
+      { challenges.map((c) => <ListItem key={ c.id } target={ route('view-challenge-submissions', c.id) }>
         <div className="flex-grow pr-5">
           <p className="text-xl text-medium">{ c.name }</p>
           <p className="text-sm">{ c.description }</p>
         </div>
-        <div className="flex-none">
-          <div className="w-8 rounded-full text-center text-neutral-100 text-medium text-sm p-2 bg-purple-600">
-            <p>{ c.points }</p>
+        <div className="flex-none flex">
+          <div className="w-8 rounded-xl text-center text-neutral-100 text-medium text-sm p-2 bg-emerald-600">
+            <Link href={ route('edit-challenge', c.id) }><PencilIcon /></Link>
           </div>
         </div>
       </ListItem>) }
