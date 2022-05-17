4017a76200715d8b8516b886f6c9675cd934fae6
Owen Jones <owen@owenjones.net>
2022-05-18 00:41:22 +0100

    changed how teams are parsed to enable leaderboard to work

--- src/resources/js/layouts/admin/team-list.js
+++ src/resources/js/layouts/admin/team-list.js
@@ -4,35 +4,42 @@ import Frame from '@/layouts/admin/frame';
 import ListItem from '@/components/admin/list-item';
 import { XIcon } from '@heroicons/react/solid';
 
-export default function List({ teams, children=false }) {
+export default function List({ title="Teams", teams, children=false, simple=false }) {
   return (
-    <Frame title="Teams">
+    <Frame title={ title }>
       { children &&
         <div className="p-3 bg-blue-600 text-neutral-100">
           <p className="text-lg">{ children }</p>
         </div>
       }
-      { (teams.length > 0) && teams.map((t) => <ListItem key={ t.id }>
+      { (Object.keys(teams).length > 0) && Object.keys(teams).map((key) => <ListItem key={ teams[key].id }>
         <div className="flex-grow pr-5">
-          <Link href={ route('view-team-submissions', t.id) }>
-            <p className="font-serif text-xl text-medium text-blue-800">{ t.name }</p>
-            <p className="text-sm">{ t.group }</p>
+          <Link href={ route('view-team-submissions', teams[key].id) }>
+            <p className="font-serif text-xl text-medium text-blue-800">{ teams[key].name }</p>
+            <p className="text-sm">{ teams[key].group }</p>
           </Link>
         </div>
         <div className="flex-none flex items-center">
-        { (t.submissions > 0) &&
-          <div className="w-8 mr-2 rounded-full text-center text-neutral-100 text-medium text-sm p-2 bg-orange-600">
-            <p>{ t.submissions }</p>
+        { (teams[key].points > 0) &&
+          <div className="w-8 mr-2 rounded-full text-center text-neutral-100 text-medium text-sm p-2 bg-green-600">
+            <p>{ teams[key].points }</p>
           </div>
         }
-          <Link href={ route('delete-team', t.id) }>
+        { (teams[key].submissions > 0) &&
+          <div className="w-8 mr-2 rounded-full text-center text-neutral-100 text-medium text-sm p-2 bg-orange-600">
+            <p>{ teams[key].submissions }</p>
+          </div>
+        }
+        { !simple && 
+          <Link href={ route('delete-team', teams[key].id) }>
             <div className="w-8 rounded-xl text-center text-neutral-100 text-medium text-sm p-2 bg-red-600">
               <XIcon />
             </div>
           </Link>
+        }
         </div>
       </ListItem>) }
-      { (teams.length === 0) && 
+      { (Object.keys(teams).length === 0) && 
         <div className="p-5 text-center">
           <p className="text-medium text-xl">No teams</p>
         </div>
