8f893c16390b7bf3b488b4cad8e9bc40be6bb605
Owen Jones <owen@owenjones.net>
2022-05-18 00:39:55 +0100

    hide the remember token, calculate points from accepted submissions

--- src/app/Models/Team.php
+++ src/app/Models/Team.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class Team extends Authenticatable {
 
   public $fillable = ['name', 'group_id'];
+  protected $hidden = ['remember_token'];
 
   public function group() {
     return $this->belongsTo(Group::class);
@@ -14,6 +15,10 @@ class Team extends Authenticatable {
 
   public function submissions() {
     return $this->hasMany(Submission::class);
+  }
+  
+  protected function getPointsAttribute() {
+    return $this->submissions()->where('accepted', true)->join('challenges', 'challenges.id', '=', 'submissions.challenge_id')->sum('challenges.points');
   }
 
 }
