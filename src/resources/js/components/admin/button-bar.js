b8588a99b180cf3c57bd99a6890fa72c8c3feba7
Owen Jones <owen@owenjones.net>
2022-05-18 16:44:53 +0100

    tweaked padding and margins to allow for multiple buttons

--- src/resources/js/components/admin/button-bar.js
+++ src/resources/js/components/admin/button-bar.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Link } from '@inertiajs/inertia-react';
+import { Link, usePage } from '@inertiajs/inertia-react';
+
 
 export function ButtonBar({ children }) {
   return (
-    <div className="p-3 flex justify-end bg-blue-600">
+    <div className="p-2 flex justify-end bg-blue-600">
       { children }
     </div>
   );
@@ -11,6 +12,8 @@ export function ButtonBar({ children }) {
 
 export function Button({ href, children }) {
   return (
-    <Link className="inline-flex items-center px-4 py-2 bg-white border border-transparent rounded-md font-semibold text-xs text-blue-900 uppercase tracking-widest active:bg-white transition ease-in-out duration-150" href={ href }>{ children }</Link>
+    <Link className="inline-flex items-center px-4 py-2 ml-2 border border-transparent
+      rounded-md font-semibold text-xs uppercase tracking-widest transition
+      ease-in-out duration-150 text-blue-900 bg-white" href={ href }>{ children }</Link>
   )
 }
