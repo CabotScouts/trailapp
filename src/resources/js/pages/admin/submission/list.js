1d6237f44664d34255e988c3c326be51153bcca0
Owen Jones <owen@owenjones.net>
2022-05-18 16:47:07 +0100

    added buttons to switch between all and pending submissions

--- src/resources/js/pages/admin/submission/list.js
+++ src/resources/js/pages/admin/submission/list.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import List from '@/layouts/admin/submission-list';
+import { Button, ButtonBar } from '@/components/admin/button-bar';
 
 export default function Submissions({ submissions }) {
   return (
-    <List submissions={ submissions } />
+    <List submissions={ submissions }>
+      <ButtonBar>
+        <Button href={ route('submissions') }>All</Button>
+        <Button href={ route('submissions', 'pending') }>Pending</Button>
+      </ButtonBar>
+    </List>
   )
 }
