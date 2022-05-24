6b1985f65959e516841281a817596c6019f36de6
Owen Jones <owen@owenjones.net>
2022-05-24 01:06:57 +0100

    added php-redis package to build, pusher-php-server to app, and updated configuration files

--- app/config/broadcasting.php
+++ app/config/broadcasting.php
@@ -37,7 +37,9 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
+                'encrypted' => true,
+                'host' => env('PUSHER_HOST'),
+                'port' => env('PUSHER_PORT', 6001),
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
