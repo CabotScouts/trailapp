3f4486f61c97c14683be90c272c890fa926ebad5
Owen Jones <owen@owenjones.net>
2022-05-24 15:11:44 +0100

    added pusher-js and laravel-echo packages; created Echo connection and listener for test events

--- app/resources/js/bootstrap.js
+++ app/resources/js/bootstrap.js
@@ -1,3 +1,20 @@
+import Echo from 'laravel-echo';
+
 window._ = require('lodash');
 window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+  broadcaster: 'pusher',
+  key: process.env.MIX_PUSHER_APP_KEY,
+  wsHost: process.env.MIX_PUSHER_HOST,
+  wsPort: process.env.MIX_PUSHER_PORT,
+  wssPort: process.env.MIX_PUSHER_PORT,
+  forceTLS: false,
+  encrypted: true,
+  disableStats: true,
+  enabledTransports: ['ws', 'wss'],
+});
+
+window.Echo.channel('global').listenToAll((e, d) => { console.log(e, d) });
