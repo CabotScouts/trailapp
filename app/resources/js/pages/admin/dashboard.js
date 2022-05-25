3ef81abce5f14b4ad2d1c7020725e4f8ea3376a7
Owen Jones <owen@owenjones.net>
2022-05-25 21:44:29 +0100

    tweaked colours so we get a spectrum again

--- app/resources/js/pages/admin/dashboard.js
+++ app/resources/js/pages/admin/dashboard.js
@@ -6,14 +6,14 @@ export default function Dashboard(props) {
   return (
     <Frame title="Dashboard" back="false">
       <Grid>
-        <GridItem className="bg-rose-600" href={ route('broadcast') }>Broadcast</GridItem>
+        <GridItem className="bg-blue-600" href={ route('broadcast') }>Broadcast</GridItem>
         <GridItem className="bg-teal-600" href={ route('submissions') }>Submissions</GridItem>
         
         <GridItem className="bg-sky-600" href={ route('leaderboard') }>Leaderboard</GridItem>
-        <GridItem className="bg-orange-600" href={ route('teams') }>Teams</GridItem>
+        <GridItem className="bg-emerald-600" href={ route('teams') }>Teams</GridItem>
         
-        <GridItem className="bg-emerald-600" href={ route('questions') }>Questions</GridItem>
-        <GridItem className="bg-green-600" href={ route('challenges') }>Challenges</GridItem>
+        <GridItem className="bg-green-600" href={ route('questions') }>Questions</GridItem>
+        <GridItem className="bg-orange-600" href={ route('challenges') }>Challenges</GridItem>
         
         <GridItem className="bg-red-600" href={ route('groups') }>Groups</GridItem>
         <GridItem className="bg-pink-600" href={ route('users') }>Users</GridItem>
