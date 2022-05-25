5ecfe1e0150efa5398f6dd6e132d13a60d51b2a1
Owen Jones <owen@owenjones.net>
2022-05-25 21:38:42 +0100

    shuffled dashboard order to pair up common modules; added logout link

--- app/resources/js/pages/admin/dashboard.js
+++ app/resources/js/pages/admin/dashboard.js
@@ -7,16 +7,21 @@ export default function Dashboard(props) {
     <Frame title="Dashboard" back="false">
       <Grid>
         <GridItem className="bg-rose-600" href={ route('broadcast') }>Broadcast</GridItem>
-        <GridItem className="bg-sky-600" href={ route('leaderboard') }>Leaderboard</GridItem>
         <GridItem className="bg-teal-600" href={ route('submissions') }>Submissions</GridItem>
+        
+        <GridItem className="bg-sky-600" href={ route('leaderboard') }>Leaderboard</GridItem>
+        <GridItem className="bg-orange-600" href={ route('teams') }>Teams</GridItem>
+        
         <GridItem className="bg-emerald-600" href={ route('questions') }>Questions</GridItem>
         <GridItem className="bg-green-600" href={ route('challenges') }>Challenges</GridItem>
-        <GridItem className="bg-orange-600" href={ route('teams') }>Teams</GridItem>
+        
         <GridItem className="bg-red-600" href={ route('groups') }>Groups</GridItem>
-        <GridItem className="bg-pink-600" href={ route('users') }>Users</GridItem> 
+        <GridItem className="bg-pink-600" href={ route('users') }>Users</GridItem>
+        
+        <GridItem className="bg-zinc-600" href={ route('logout') }>Logout</GridItem> 
       </Grid>
     </Frame>
   );
 }
  
-// <GridItem className="bg-rose-600" href={ route('groups') }>Settings</GridItem>
+// <GridItem className="bg-slate-600" href={ route('groups') }>Settings</GridItem>
