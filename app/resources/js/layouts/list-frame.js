18ab804309d3fe67212998672aeb3e1076bca271
Owen Jones <owen@owenjones.net>
2022-06-05 02:13:55 +0100

    made tab bg colour slightly lighter

--- app/resources/js/layouts/list-frame.js
+++ app/resources/js/layouts/list-frame.js
@@ -27,14 +27,14 @@ export default function ListFrame({ team, group, points, children }) {
       </div>
       
       <div className="flex-none w-full fixed flex items-stretch bottom-0 h-14 font-medium font-serif text-xl text-purple-900">
-        <Link href={ route('trail') } className={`flex-auto h-full ${ component === 'question/list' ? 'bg-slate-900' : 'bg-neutral-100' }`} preserveScroll>
-          <div className={`h-full w-full flex items-center border-transparent ${ component === 'question/list' ? 'bg-neutral-100 rounded-b-lg' : 'bg-slate-900 rounded-tr-lg text-neutral-100' }`}>
+        <Link href={ route('trail') } className={`flex-auto h-full ${ component === 'question/list' ? 'bg-slate-800' : 'bg-neutral-100' }`} preserveScroll>
+          <div className={`h-full w-full flex items-center border-transparent ${ component === 'question/list' ? 'bg-neutral-100 rounded-b-lg' : 'bg-slate-800 rounded-tr-lg text-neutral-100' }`}>
             <div className="mx-auto">Questions</div>
           </div>
         </Link>
         
-        <Link href={ route('trail-challenges') } className={`flex-auto h-full ${ component === 'challenge/list' ? 'bg-slate-900' : 'bg-neutral-100' }`} preserveScroll>
-        <div className={`h-full w-full flex items-center border-transparent ${ component === 'challenge/list' ? 'bg-neutral-100 rounded-b-lg' : 'bg-slate-900 rounded-tl-lg text-neutral-100' }`}>
+        <Link href={ route('trail-challenges') } className={`flex-auto h-full ${ component === 'challenge/list' ? 'bg-slate-800' : 'bg-neutral-100' }`} preserveScroll>
+        <div className={`h-full w-full flex items-center border-transparent ${ component === 'challenge/list' ? 'bg-neutral-100 rounded-b-lg' : 'bg-slate-800 rounded-tl-lg text-neutral-100' }`}>
           <div className="mx-auto">Challenges</div>
         </div>
         </Link>
