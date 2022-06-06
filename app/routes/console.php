996044f0c52a58abf3dd5befa18c4c8bfbf07a0e
Owen Jones <owen@owenjones.net>
2022-06-06 15:51:37 +0100

    added new node container to build react app; bridged console command to make new users to build script

--- app/routes/console.php
+++ app/routes/console.php
@@ -7,18 +7,19 @@ use Illuminate\Support\Str;
 
 use App\Models\User;
 
-Artisan::command("user:make {username} {--password}", function ($username, $password=false) {
-  $p = $password ? $this->secret("Enter a password") : Str::random(10);
+Artisan::command("user:make {username?} {--random}", function ($username=false, $random=false) {
+  $u = $username ? $username : $this->ask("Enter a username");
+  $p = $random ? Str::random(10) : $this->secret("Enter a password") ;
 
   User::create([
-    'username' => $username,
+    'username' => $u,
     'password' => Hash::make($p),
   ]);
 
-  if($password) {
-      $this->info("Created new user {$username}");
+  if($random) {
+    $this->info("Created new user {$u} with password {$p}");
   }
   else {
-      $this->info("Created new user {$username} with password {$p}");
+    $this->info("Created new user {$u}");
   }
 });
