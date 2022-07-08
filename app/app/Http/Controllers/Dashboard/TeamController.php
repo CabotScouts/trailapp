e497f16c1cbe698579bbb1d4574821bd005c1e32
Owen Jones <owen@owenjones.net>
2022-07-08 17:23:44 +0100

    started shuffling for pagination

--- app/app/Http/Controllers/Dashboard/TeamController.php
+++ app/app/Http/Controllers/Dashboard/TeamController.php
@@ -14,7 +14,7 @@ class TeamController extends Controller {
 
   public function broadcast(Request $request, $id = false) {
     $team = ($id) ? Team::where('id', $id)->firstOrFail() : false;
-    
+
     if($request->isMethod('get')) {
       return Inertia::render('admin/broadcast', [
         'id' => ($team) ? $team->id : null,
@@ -25,7 +25,7 @@ class TeamController extends Controller {
       $data = $request->validate([
         'message' => 'required'
       ]);
-      
+
       MessageToTeams::dispatch($request->message, $id);
       $route = $id ? 'teams' : 'dashboard';
       return redirect()->route($route);
@@ -45,7 +45,7 @@ class TeamController extends Controller {
 
   public function viewTeamSubmissions($id) {
     $team = Team::with(['group', 'submissions' => function($query) use ($id) {
-      $query->where('team_id', $id)->orderBy('created_at', 'desc');
+      $query->where('team_id', $id)->orderBy('created_at', 'desc')->paginate(10);
     }, 'submissions.upload', 'submissions.challenge', 'submissions.question'])->findOrFail($id);
 
     return Inertia::render('admin/team/submissions', [
@@ -53,15 +53,7 @@ class TeamController extends Controller {
         'name' => $team->name,
         'group' => $team->group->name,
       ],
-      'submissions' => $team->submissions->map(fn($submission) => [
-        'id' => $submission->id,
-        'upload' => ($submission->upload) ? [ 'file' => $submission->upload->file, 'link' => $submission->upload->link ] : false,
-        'answer' => ($submission->answer) ? $submission->answer : false,
-        'challenge' => ($submission->challenge) ? $submission->challenge->name : false,
-        'question' => ($submission->question) ? ['name' => $submission->question->name, 'text' => $submission->question->question] : false,
-        'time' => $submission->time,
-        'accepted' => $submission->accepted,
-      ]),
+      'submissions' => $team->submissions,
     ]);
   }
 
@@ -84,5 +76,5 @@ class TeamController extends Controller {
       }
     }
   }
-  
+
 }
