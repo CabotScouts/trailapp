03d40088c92e72690b58b5d12afc0d8f215d59ab
Owen Jones <owen@owenjones.net>
2022-08-03 15:59:37 +0100

    added new option to filter leaderboard by group

--- app/resources/js/pages/admin/leaderboard.js
+++ app/resources/js/pages/admin/leaderboard.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Inertia } from '@inertiajs/inertia'
 import Frame from '@/layouts/admin/frame';
 import List from '@/layouts/admin/team-list';
 
-export default function TeamList({ teams }) {
+const setFilter = (e) => {
+  Inertia.get(route('leaderboard', e.target.value));
+};
+
+export default function TeamList({ groups, teams, filter }) {
   return (
-    <List title="Leaderboard" teams={ teams } simple />
+    <List title="Leaderboard" teams={ teams } simple>
+    <div className="flex w-full items-center">
+      <div className="grow text-right mr-2">Filter by group</div>
+      <div className="flex-none">
+        <form>
+          <select
+          name="group"
+          className="border-slate-300 focus:border-purple-300 focus:ring focus:ring-purple-200 focus:ring-opacity-50 rounded-md shadow-sm text-black"
+          onChange={ setFilter }
+          >
+          <option key="all" value="" selected={( filter === false )}>Show all groups</option>
+          { groups.map(g => (<option key={ g.id } value={ g.id } selected={( g.id == filter )}>{ g.name }</option>)) }
+          </select>
+        </form>
+      </div>
+    </div>
+    </List>
   )
 }
