ea53656c22e029d125c866a9b78f305aacb3d0cb
Owen Jones <owen@owenjones.net>
2023-04-06 21:05:14 +0100

    no longer need to specify exactly which properties we're passing

--- app/resources/js/components/form/input.js
+++ app/resources/js/components/form/input.js
@@ -1,18 +1,14 @@
 import React from 'react';
 
-export default function Input({ title, type, name, onChange, required, placeholder, value }) {
+export default function Input(props) {
   return (
     <div>
-      <label htmlFor="name" className="block font-medium text-sm text-slate-700">{ title }</label>
+      <label htmlFor="name" className="block font-medium text-sm text-slate-700">{props.title}</label>
       <div className="flex flex-col items-start">
         <input
-          type={ type }
-          name={ name }
-          value={ value }
           className="border-slate-300 focus:border-purple-300 focus:ring focus:ring-purple-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full"
-          required={ required }
-          placeholder={ placeholder }
           onChange={(e) => onChange(e)}
+          {...props}
         />
       </div>
     </div>
