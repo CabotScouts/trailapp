6e1487e44eb1a65effc5e1d3b9467dc7da59a9f0
Owen Jones <owen@owenjones.net>
2023-04-06 22:30:01 +0100

    allow models to be filled

--- app/app/Models/Challenge.php
+++ app/app/Models/Challenge.php
@@ -7,6 +7,7 @@ use App\Traits\TakesSubmission;
 
 class Challenge extends Model {
   use TakesSubmission;
+  protected $fillable = ['name', 'description', 'points'];
   public $timestamps = false;
 
   public function event() {
--- app/app/Models/Question.php
+++ app/app/Models/Question.php
@@ -7,6 +7,7 @@ use App\Traits\TakesSubmission;
 
 class Question extends Model {
   use TakesSubmission;
+  protected $fillable = ['number', 'name', 'question', 'points'];
   public $timestamps = false;
 
   public function event() {
