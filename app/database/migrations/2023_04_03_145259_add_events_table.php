ee7635a4c803fe91280944d8a1919f93e3c8a358
Owen Jones <owen@owenjones.net>
2023-04-06 19:10:53 +0100

    events should be in-active by default

--- app/database/migrations/2023_04_03_145259_add_events_table.php
+++ app/database/migrations/2023_04_03_145259_add_events_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->boolean('active');
+            $table->boolean('active')->default(false);
         });
     }
 
