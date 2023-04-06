c090348f923e85d830a459b487c295b50be503c2
Owen Jones <owen@owenjones.net>
2023-04-06 19:43:31 +0100

    adding concept of an evening being 'running' - will allow events to be setup/edited before they can be played

--- app/database/migrations/2023_04_03_145259_add_events_table.php
+++ app/database/migrations/2023_04_03_145259_add_events_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->boolean('active')->default(false);
+            $table->boolean('running')->default(false);
         });
     }
 
