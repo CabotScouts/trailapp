fcfbbe216a16a6b6069f43c3056d4e24ab212efa
Owen Jones <owen@owenjones.net>
2025-12-15 23:51:31 +0000

    include new vite files, and also ignore them in git

--- app/resources/views/app.blade.php
+++ app/resources/views/app.blade.php
@@ -1,20 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name') }}</title>
-        <link rel="shortcut icon" type="image/png" href="{{ asset('favicon.png') }}"/>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&family=Staatliches&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        @routes
-        <script src="{{ mix('js/app.js') }}" defer></script>
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased bg-slate-900 overscroll-none">
-        @inertia
-    </body>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title inertia>{{ config('app.name') }}</title>
+  <link rel="shortcut icon" type="image/png" href="{{ asset('favicon.png') }}" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&family=Staatliches&display=swap"
+    rel="stylesheet">
+  @viteReactRefresh
+  @vite('resources/js/app.jsx')
+  @routes
+  @inertiaHead
+</head>
+
+<body class="font-sans antialiased bg-slate-900 overscroll-none">
+  @inertia
+</body>
+
 </html>
