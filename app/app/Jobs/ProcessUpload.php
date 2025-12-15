2d4b5e53472e864ca0dbc6562c09cf33ba41659c
Owen Jones <owen@owenjones.net>
2025-12-15 00:18:56 +0000

    updated to reflect library changes

--- app/app/Jobs/ProcessUpload.php
+++ app/app/Jobs/ProcessUpload.php
@@ -10,7 +10,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Storage;
 
-use Intervention\Image\Facades\Image;
+use Intervention\Image\Laravel\Facades\Image;
 
 use App\Models\Upload;
 
@@ -24,24 +24,15 @@ class ProcessUpload implements ShouldQueue {
   }
 
   public function handle() {
-    $image = Image::make(Storage::path("public/uploads/0/{$this->upload->filename}"));
-    $image = $image->orientate();
+    $image = Image::read(Storage::path("public/uploads/0/{$this->upload->filename}"));
     $width = $image->width();
     $height = $image->height();
-    
-    $landscape = ($width > $height);
-    
-    if($landscape && $width > 1920) {
-      $image = $image->resize(1920, null, function ($constraint) {
-        $constraint->aspectRatio();
-        $constraint->upsize();
-      });
+
+    if(($width > $height) && $width > 1920) {
+      $image = $image->scaleDown(width: 1920);
     }
     else if($height > 1080) {
-      $image = $image->resize(null, 1080, function ($constraint) {
-        $constraint->aspectRatio();
-        $constraint->upsize();
-      });
+      $image = $image->scaleDown(height: 1080);
     }
     
     $image->save(Storage::path("public/uploads/1/{$this->upload->filename}"), 75);
