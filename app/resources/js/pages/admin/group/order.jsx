dfd03c869ebdf9d659701f4e52befe87ec2a9e13
Owen Jones <owen@owenjones.net>
2025-12-16 12:33:32 +0000

    skeleton for group ordering view

--- app/resources/js/pages/admin/group/order.jsx
+++ app/resources/js/pages/admin/group/order.jsx
@@ -1,28 +1,42 @@
 import React from 'react';
+import { useForm } from '@inertiajs/react';
+import { DndContext, useDraggable, useDroppable } from '@dnd-kit/core';
 import Frame, { Container } from '@/layouts/admin/frame';
 import ListItem from '@/components/admin/list-item';
 import { Button, ButtonBar } from '@/components/admin/button-bar';
 
 export default function List({ groups }) {
+  const { data, setData, post, processing, errors, reset } = useForm({
+  });
+
+  const submit = (e) => {
+    e.preventDefault();
+    post(route('order-groups'));
+  };
+
   return (
     <Frame title="Groups">
       <ButtonBar>
-        <Button href={route('add-group')}>Save Order</Button>
+        <form onSubmit={submit}>
+          <Button processing={processing}>Save Order</Button>
+        </form>
       </ButtonBar>
 
-      <Container>
-        {groups.map((g) => <ListItem key={g.id}>
-          <div className="flex-grow pr-5">
-            <p className="text-xl text-medium">{g.name}</p>
-          </div>
-        </ListItem>)}
+      <DndContext>
+        <Container>
+          {groups.map((g) => <ListItem key={g.id}>
+            <div className="flex-grow pr-5">
+              <p className="text-xl text-medium">{g.name}</p>
+            </div>
+          </ListItem>)}
 
-        {(groups.length === 0) &&
-          <div className="p-5 text-center">
-            <p className="text-medium text-xl">No groups</p>
-          </div>
-        }
-      </Container>
+          {(groups.length === 0) &&
+            <div className="p-5 text-center">
+              <p className="text-medium text-xl">No groups</p>
+            </div>
+          }
+        </Container>
+      </DndContext>
     </Frame>
   )
 }
