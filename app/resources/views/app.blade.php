d7a58589c4c3a11397e99c5bdfe73217abbe768a
Owen Jones <owen@owenjones.net>
2025-12-16 00:29:24 +0000

    we now load fonts directly in the css

--- app/resources/views/app.blade.php
+++ app/resources/views/app.blade.php
@@ -4,13 +4,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
   <title inertia>{{ config('app.name') }}</title>
   <link rel="shortcut icon" type="image/png" href="{{ asset('favicon.png') }}" />
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&family=Staatliches&display=swap"
-    rel="stylesheet">
   @viteReactRefresh
   @vite('resources/js/app.jsx')
   @routes
