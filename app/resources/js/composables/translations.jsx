2bbb384d6c4970d8ce387913a1cc8a0c4c216764
Owen Jones <owen@owenjones.net>
2025-12-18 01:03:31 +0000

    joining here ruins injected dom elements, will have to join at the individual points this being an array messes things up

--- app/resources/js/composables/translations.jsx
+++ app/resources/js/composables/translations.jsx
@@ -3,5 +3,7 @@ import reactStringReplace from 'react-string-replace';
 
 export function __(value, attributes, array = usePage().props.translations) {
   const string = array[value] != null ? array[value] : value;
-  return reactStringReplace(string, /(:[\w]+)/g, (m, _) => attributes[m.replace(":", "")] || m).join("");
+  const t = reactStringReplace(string, /(:[\w]+)/g, (m, _) => attributes[m.replace(":", "")] || m);
+  console.log(t);
+  return t;
 }
