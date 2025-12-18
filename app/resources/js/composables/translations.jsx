a34bd42fd8af2fbd379efb7f0bcca46e7a2f2f24
Owen Jones <owen@owenjones.net>
2025-12-18 00:31:11 +0000

    turns out this returned as an array, which only showed up in some elements

--- app/resources/js/composables/translations.jsx
+++ app/resources/js/composables/translations.jsx
@@ -3,5 +3,5 @@ import reactStringReplace from 'react-string-replace';
 
 export function __(value, attributes, array = usePage().props.translations) {
   const string = array[value] != null ? array[value] : value;
-  return reactStringReplace(string, /(:[\w]+)/g, (m, _) => attributes[m.replace(":", "")] || m);
+  return reactStringReplace(string, /(:[\w]+)/g, (m, _) => attributes[m.replace(":", "")] || m).join("");
 }
