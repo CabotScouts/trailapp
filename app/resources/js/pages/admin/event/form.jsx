4950ffa4547f4d6c0ec341e399ae9723ef5ce0ff
Owen Jones <owen@owenjones.net>
2025-12-18 02:17:28 +0000

    events module translated

--- app/resources/js/pages/admin/event/form.jsx
+++ app/resources/js/pages/admin/event/form.jsx
@@ -9,11 +9,12 @@ import Input from '@/components/form/input';
 import Button from '@/components/form/button';
 import Select from '@/components/form/select';
 import Checkbox from '@/components/form/checkbox';
+import { __ } from '@/composables/translations';
 
 export default function AddEvent(props) {
 
   const add = (props.event === false);
-  const action = add ? 'Add' : 'Edit';
+  const action = add ? __("Add Event") : __("Edit Event");
 
   const { data, setData, post, processing, errors, reset } = useForm({
     id: props.event.id || '',
@@ -45,32 +46,32 @@ export default function AddEvent(props) {
 
   return (
     <>
-      <Head title={`${action} Event`} />
+      <Head title={action} />
       <Modal back={route('events')}>
         <div className="p-10 pt-20">
           <div className="p-5 bg-white rounded-xl shadow-lg w-full">
-            <Header title={`${action} Event`} />
+            <Header title={action} />
             <Errors errors={errors} />
 
             <Group onSubmit={submit}>
-              <Input type="text" title="Event Name" name="name" placeholder="Name" value={data.name} onChange={handleChange} required />
+              <Input type="text" title={__("Event Name")} name="name" placeholder={__("Name")} value={data.name} onChange={handleChange} required />
               {(add == true) &&
                 <>
-                  <Checkbox name="clone" label="Clone an existing event?" onChange={handleCheckbox} defaultChecked={data.clone} />
-                  <Select title="Event to clone" name="clone_event_id" onChange={handleChange}>
-                    <option value="-">Select an Event</option>
+                  <Checkbox name="clone" label={__("Clone an existing event?")} onChange={handleCheckbox} defaultChecked={data.clone} />
+                  <Select title={__("Event to clone")} name="clone_event_id" onChange={handleChange}>
+                    <option value="-">{__("Select an Event")}</option>
                     {props.events.map(e => (<option key={e.id} value={e.id}>{e.name}</option>))}
                   </Select>
-                  <Checkbox name="clone_questions" label="Clone questions" onChange={handleCheckbox} defaultChecked={data.clone_questions} />
-                  <Checkbox name="clone_challenges" label="Clone challenges" onChange={handleCheckbox} defaultChecked={data.clone_challenges} />
-                  <Checkbox name="clone_groups" label="Clone groups" onChange={handleCheckbox} defaultChecked={data.clone_groups} />
+                  <Checkbox name="clone_questions" label={__("Clone questions")} onChange={handleCheckbox} defaultChecked={data.clone_questions} />
+                  <Checkbox name="clone_challenges" label={__("Clone challenges")} onChange={handleCheckbox} defaultChecked={data.clone_challenges} />
+                  <Checkbox name="clone_groups" label={__("Clone groups")} onChange={handleCheckbox} defaultChecked={data.clone_groups} />
                 </>
               }
-              <Button processing={processing}>{`${action} Event`}</Button>
+              <Button processing={processing}>{action}</Button>
             </Group>
             {(add == false) && (props.event.active == false) &&
               <div className="pt-2">
-                <Link href={route('delete-event', props.event.id)} type="button" className="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest">Delete Event</Link>
+                <Link href={route('delete-event', props.event.id)} type="button" className="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest">{__("Delete Event")}</Link>
               </div>
             }
           </div>
--- app/resources/js/pages/admin/event/toggle-active.jsx
+++ app/resources/js/pages/admin/event/toggle-active.jsx
@@ -4,6 +4,7 @@ import { Modal } from '@/layouts/modal';
 import Group from '@/layouts/form/group';
 import Header from '@/components/form/header';
 import Errors from '@/components/form/errors';
+import { __ } from '@/composables/translations';
 
 export default function ToggleActiveEvent({ event }) {
 
@@ -18,18 +19,18 @@ export default function ToggleActiveEvent({ event }) {
 
   return (
     <>
-      <Head title="Change Active Event" />
+      <Head title={__("Change Active Event")} />
       <Modal back={route('events')}>
         <div className="p-10 pt-20">
           <div className="p-5 bg-white rounded-xl shadow-lg w-full">
-            <Header title="Change Active Event">
-              <p className="font-medium mb-2">Are you sure you want to make <span className="font-bold">{event.name}</span> the active event?</p>
-              <p className="text-red-500">This will end the currently running event, any teams currently participating will be locked out of the event.</p>
+            <Header title={__("Change Active Event")}>
+              <p className="font-medium mb-2">{__("change_active_check", { event: <span className="font-bold">{event.name}</span> })}</p>
+              <p className="text-red-500">{__("change_active_outcome")}</p>
             </Header>
             <Errors errors={errors} />
             <Group onSubmit={toggleActive}>
               <input type="hidden" name="id" value={event.id} />
-              <button type="submit" className="inline-flex items-center px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest">Make Active Event</button>
+              <button type="submit" className="inline-flex items-center px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest">{__("Make Active Event")}</button>
             </Group>
           </div>
         </div>
