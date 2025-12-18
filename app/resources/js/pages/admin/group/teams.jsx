38e65ebb71e9692daf361f72c582ec1feec85676
Owen Jones <owen@owenjones.net>
2025-12-18 01:49:32 +0000

    groups module translated

--- app/resources/js/pages/admin/group/teams.jsx
+++ app/resources/js/pages/admin/group/teams.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Frame, { Stripe } from '@/layouts/admin/frame';
 import List from '@/layouts/admin/team-list';
+import { __ } from '@/composables/translations';
 
 export default function TeamList({ teams, group }) {
   return (
-    <List teams={ teams }>
-      Teams in <span className="font-bold">{ group }</span>
+    <List teams={teams}>
+      {__("teams_in", { group: <span className="font-bold">{group}</span> })}
     </List>
   )
 }
