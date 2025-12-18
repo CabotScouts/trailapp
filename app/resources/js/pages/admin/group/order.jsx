d44a71abdc3f617f69ae925f202aa6352b938f34
Owen Jones <owen@owenjones.net>
2025-12-18 15:04:16 +0000

    brought in sort library and sketched out sorting list of groups

--- app/resources/js/pages/admin/group/order.jsx
+++ app/resources/js/pages/admin/group/order.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from '@inertiajs/react';
-import { DndContext, useDraggable, useDroppable } from '@dnd-kit/core';
+import {
+  DndContext,
+  DragOverlay,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core';
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+
 import Frame, { Container } from '@/layouts/admin/frame';
 import ListItem from '@/components/admin/list-item';
 import { Button, ButtonBar } from '@/components/admin/button-bar';
@@ -10,10 +25,48 @@ export default function List({ groups }) {
   const { data, setData, post, processing, errors, reset } = useForm({
   });
 
+  const [activeId, setActiveId] = useState(null);
+  const [items, setItems] = useState([1, 2, 3]);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
   const submit = (e) => {
     e.preventDefault();
     post(route('order-groups'));
   };
+
+  function handleDragStart(event) {
+    const { active } = event;
+
+    setActiveId(active.id);
+  }
+
+  function handleDragEnd(event) {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(active.id);
+        const newIndex = items.indexOf(over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  }
+
+  function SortableItem({ group }) {
+    return (
+      <ListItem key={group.id} id={group.id}>
+        <div className="flex-grow pr-5">
+          <p className="text-xl text-medium">{group.name}</p>
+        </div>
+      </ListItem>
+    )
+  }
 
   return (
     <Frame title={__("Reorder Groups")}>
@@ -23,21 +76,34 @@ export default function List({ groups }) {
         </form>
       </ButtonBar>
 
-      <DndContext>
-        <Container>
-          {groups.map((g) => <ListItem key={g.id}>
-            <div className="flex-grow pr-5">
-              <p className="text-xl text-medium">{g.name}</p>
-            </div>
-          </ListItem>)}
+      <Container>
+        {(groups.length === 0) &&
+          <div className="p-5 text-center">
+            <p className="text-medium text-xl">{__("No groups")}</p>
+          </div>
+        }
 
-          {(groups.length === 0) &&
-            <div className="p-5 text-center">
-              <p className="text-medium text-xl">{__("No groups")}</p>
-            </div>
-          }
-        </Container>
-      </DndContext>
+        {(groups.length > 0) &&
+          <DndContext
+            sensors={sensors}
+            collisionDetection={closestCenter}
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+          >
+            <SortableContext
+              items={items}
+              strategy={verticalListSortingStrategy}
+            >
+              {groups.map((g) => <SortableItem group={g} />)}
+            </SortableContext>
+
+            <DragOverlay>
+
+            </DragOverlay>
+          </DndContext>
+        }
+      </Container>
+
     </Frame>
   )
 }
